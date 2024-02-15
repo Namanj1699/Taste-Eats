f4da2f04a50db76fb0226a538951fb466e6d9bdb
Naman Jain <jainnaman1699@gmail.com>
2024-02-16 01:51:05 +0530

    Deleted Files

--- src/components/Contact.js
+++
@@ -1,6 +0,0 @@
-const Contact = () =>{
-    return(
-    <h1>I am inside contact component</h1>);
-}
-
-export default Contact;
--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTThk6B4F2Q-aQEosit-VHmYfxhiDDAlBNo-JuA8nQN&s"
-
-export const RESMENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6939481&lng=77.2981474&restaurantId="
