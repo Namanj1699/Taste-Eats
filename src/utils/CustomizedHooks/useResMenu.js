dd2eb39926b7faa990848ad9d4f533806c18f198
Naman Jain <jainnaman1699@gmail.com>
2024-02-19 20:55:04 +0530

    Resolving CORS Error

--- src/utils/CustomizedHooks/useResMenu.js
+++ src/utils/CustomizedHooks/useResMenu.js
@@ -9,7 +9,7 @@ const useResMenu = (resId) => {
   }, []);
 
   const fetchMenu = async () => {
-    const response = await fetch(RESMENU_API + resId);
+    const response = await fetch("https://corsproxy.org/?"+RESMENU_API + resId);
     const json = await response.json();
     setResInfo(json.data);
   };
