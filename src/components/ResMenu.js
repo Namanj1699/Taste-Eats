075a79184526507b6a40878b932bf64c29cfde2a
Naman Jain <jainnaman1699@gmail.com>
2024-04-24 17:48:10 +0530

    Code changes for fixing CORS Error by creating my own proxy on cloudfare worker.

--- src/components/ResMenu.js
+++ src/components/ResMenu.js
@@ -19,10 +19,10 @@ const ResMenu = () => {
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info;
   
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c?.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
--- src/utils/CustomizedHooks/useResMenu.js
+++ src/utils/CustomizedHooks/useResMenu.js
@@ -8,8 +8,13 @@ const useResMenu = (resId) => {
   }, []);
 
   const fetchMenu = async () => {
-    const url = 'https://corsproxy.org/?' + encodeURIComponent("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6939481&lng=77.2981474&restaurantId=") + resId;
-    const response = await fetch(url);
+    const url = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6939481&lng=77.2981474&restaurantId=";
+    const enc_url = encodeURIComponent(url);
+    
+    //Creating my own proxy and deployed on cloud fare worker to resolve CORS Error...
+    const updated_url= "https://tasteats.jainnaman1699.workers.dev/?apiUrl=" + enc_url + resId;
+    
+    const response = await fetch(updated_url);
     const json = await response.json();
     setResInfo(json.data);
   };
